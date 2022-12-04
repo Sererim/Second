499392032d5b0b8080cbbce46ac40755e37a6e59
Sererim <danielleymanw@gmail.com>
2022-12-04 16:07:45 +0300

    initial commit.

---
+++ Errors.py
@@ -0,0 +1,29 @@
+# file with a class for handling errors also has colors in it
+
+class Errors:
+    @staticmethod
+    def color(col: int):
+        colors = ('\033[95m', #Header 0
+                  '\033[94m', #Blue 1
+                  '\033[96m', #Cyan 2
+                  '\033[92m', #Green 3
+                  '\033[93m', #Warning 4
+                  '\033[91m', #Fail 5
+                  '\033[0m', #ENDC 6
+                  '\033[1m', #Bold 7
+                  '\033[4m', #Underline 8
+                  )
+        return colors[col]
+
+    @staticmethod
+    def message(msg: int):
+        answer: list = [f"NaN.\n{Errors.color(1)}Not a number was entered!{Errors.color(6)}"
+                        f""
+                        f""
+                        f""
+                        f""
+                        f""
+                        f""
+                        f""
+                        f""]
+        return answer[msg]
---
+++ first.py
@@ -0,0 +1,38 @@
+"""
+    Input: 6782
+    Output: 23
+    
+    Input: 0.56
+    Output: 11
+"""
+from Errors import Errors
+
+numbers: tuple = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.') # [0,10], len = 11
+
+
+def main():
+    enter: str = "NULL"
+    num: int = 0
+    
+    while True:
+        print("Program is running.\n")
+        enter = str(input("Enter a number.\n"))
+        for i in enter:
+            for x in numbers:
+                if i == x:
+                    num += int(x)
+                elif i not in numbers:
+                    print(f"{Errors.message(0)}")
+                    main()
+        print(f"Sum of numbers in the number entered is: {num}")
+        enter = input("Do you want to termiate the program Y/y ?\n")
+        if enter == 'Y' or enter == 'y':
+            break
+        else:
+            main()
+    return 0
+
+
+if __name__ == "__main__":
+    main()    
+    
